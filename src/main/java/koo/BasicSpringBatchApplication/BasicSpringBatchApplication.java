ec62e6d49f25e35fa4ed9f0ddf767a6db5ea4105
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-24 05:13:37 +0900

    Setting first environment & Add application.yml, docker-compose.yml

---
+++ src/main/java/koo/BasicSpringBatchApplication/BasicSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package koo.BasicSpringBatchApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koo/BasicSpringBatchApplication/BasicSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.BasicSpringBatchApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
